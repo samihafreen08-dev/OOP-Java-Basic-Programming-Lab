d116de5050e80233ba92706f3c93c8dbf45e4f91
samihafreen08-dev <samihafreen08@gmail.com>
2026-07-29 23:41:27 +0600

    Add files via upload

---
+++ OOP Java Basic Programming Lab/Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+
+    public static void main(String[] args) {
+
+        int num1 = 74;
+        int num2 = 36;
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+
+    }
+
+}
